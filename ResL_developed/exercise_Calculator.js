ebaf2384194ec5439d0d92f25abc82fdd5ed8afa
Gustavo Barboza <68360620+EncryptedAsimov@users.noreply.github.com>
2020-11-02 21:16:33 -0600

    Add files via upload

---
+++ ResL_developed/exercise_Calculator.js
@@ -0,0 +1,7 @@
+var a = Number(prompt("Give the first Number"));
+var b = Number(prompt("Give the second Number"));
+var c = a + b;
+var d = a - b;
+var e = a * b;
+
+alert("La suma, resta y multiplicación de de sus numeros secuencialmente es " + c + " " + " " + d + " "+ e);
